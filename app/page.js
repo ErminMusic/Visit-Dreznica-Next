72a4ab0f053f71c5ddaab3732acccd8b025abe45
Ermin Music <emusic444@gmail.com>
2024-02-20 20:48:02 +0100

    Fixing Analytics 1

--- app/page.js
+++ app/page.js
@@ -1,8 +1,18 @@
 import HomePage from "@/components/Home/HomePage/HomePage";
+import { Partytown } from "@builder.io/partytown/react";
+import Head from "next/head";
 
 export default function Home() {
     return (
         <>
+            <Head>
+                <Partytown debug={true} forward={["dataLayer.push"]} />
+                <script
+                    src="https://example.com/analytics.js"
+                    type="text/partytown"
+                    async
+                />
+            </Head>
             <main>
                 <HomePage />
             </main>
